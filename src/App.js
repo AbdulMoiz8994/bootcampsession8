b9b7e1eb033e1a38d4e39f8e62da93347f1fc253
AbdulMoiz8994 <moiza8994@gmail.com>
2021-02-13 02:35:51 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react'
 import './App.css';
 
 function App() {
+
+  // useEffect(() => {
+  //   fetch("https://api.github.com/users/AbdulMoiz8994/repos")
+  //     .then(response => response.json())
+  //     .then(result => {
+  //       console.log(result)
+  //       setrepos(result)
+  //     })
+
+  // },[])
+
+
+  useEffect(() =>{
+    async function getapi(){
+      const data= await fetch("https://api.github.com/users/AbdulMoiz8994/repos")
+      const jsons= await data.json()
+      console.log(jsons)
+      setrepos(jsons)
+    }
+
+getapi()
+
+  },[])
+  let [repos, setrepos] = useState([{}])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>hello world</h1>
+      <ul>
+        {repos.map((response,ind) => {
+          return (
+            <li key={ind}>
+      {response.name}
+            </li>
+          )
+        })}
+
+      </ul>
     </div>
   );
 }
