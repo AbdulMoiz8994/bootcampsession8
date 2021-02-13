ad59b723ded3617fd768da9d76887ddd70686d1e
AbdulMoiz8994 <moiza8994@gmail.com>
2021-02-13 18:44:31 +0500

    we did practice of useffect and useState

--- src/App.js
+++ src/App.js
@@ -3,6 +3,72 @@ import './App.css';
 
 function App() {
 
+  
+// useEffect(() => {
+//    fetch("https://api.github.com/users/AbdulMoiz8994/repos")  
+//    .then((response) => response.json())
+//    .then((result) => {
+//      console.log(result)
+//      setrepos(result)
+//    })
+// },[])
+//    let [repos,setrepos]=useState([])
+//   return(
+//     <div>
+//      <h1>GitHub Repositories</h1>
+//      <ul>
+//        {repos.map((list,ind) => {
+//          return(
+//            <li key={ind}>{list.name}</li>
+//          )
+//        })}
+//      </ul>
+//     </div>
+//   )
+
+
+useEffect(() =>{
+
+
+async function asynsc() {
+  try{
+    const data= await fetch("https://api.github.com/users/AbdulMoiz8994/repos") 
+    const jsons=await data.json()
+    console.log(jsons) 
+    setRepos(jsons)
+  }catch(error){
+     return error
+  }
+}
+asynsc()
+
+},[])
+let [repos,setRepos]=useState([{}])
+return(
+  <div>
+    <h1>This s all git Repositories of asynsc await</h1>
+    <ul>
+      {repos.map((listrepos,ind) =>{
+        return(
+        <li key={ind}>
+           {listrepos.name}
+        </li>
+        )
+      })}
+    </ul>
+  </div>
+)
+
+
+
+
+
+
+
+
+
+
+
   // useEffect(() => {
   //   fetch("https://api.github.com/users/AbdulMoiz8994/repos")
   //     .then(response => response.json())
@@ -14,33 +80,33 @@ function App() {
   // },[])
 
 
-  useEffect(() =>{
-    async function getapi(){
-      const data= await fetch("https://api.github.com/users/AbdulMoiz8994/repos")
-      const jsons= await data.json()
-      console.log(jsons)
-      setrepos(jsons)
-    }
+  // useEffect(() =>{
+  //   async function getapi(){
+  //     const data= await fetch("https://api.github.com/users/AbdulMoiz8994/repos")
+  //     const jsons= await data.json()
+  //     console.log(jsons)
+  //     setrepos(jsons)
+  //   }
 
-getapi()
+// getapi()
 
-  },[])
-  let [repos, setrepos] = useState([{}])
-  return (
-    <div>
-      <h1>hello world</h1>
-      <ul>
-        {repos.map((response,ind) => {
-          return (
-            <li key={ind}>
-      {response.name}
-            </li>
-          )
-        })}
+//   },[])
+//   let [repos, setrepos] = useState([{}])
+  // return (
+  //   <div>
+  //     <h1>hello world</h1>
+  //     <ul>
+  //       {repos.map((response,ind) => {
+  //         return (
+  //           <li key={ind}>
+  //     {response.name}
+  //           </li>
+  //         )
+  //       })}
 
-      </ul>
-    </div>
-  );
+  //     </ul>
+  //   </div>
+  // );
 }
 
 export default App;
